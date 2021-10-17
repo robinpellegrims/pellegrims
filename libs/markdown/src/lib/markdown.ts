b6d1246a4687c49eb3b9549eb7652c77b188c881
robinpel <robin.pellegrims@gmail.com>
2021-10-17 12:00:35 +0200

    feat(pellegrims-dev): add simple blog system

---
+++ libs/markdown/src/lib/markdown.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
+import { MarkdownDocument } from './markdown.model';
+import { markdownToHtml } from './markdown-to-html';
+
+export const getParsedFileContentBySlug = (
+  slug: string,
+  postsPath: string
+): MarkdownDocument => {
+  const postFilePath = join(postsPath, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(postFilePath);
+
+  const { data, content } = matter(fileContents);
+
+  return {
+    frontMatter: data,
+    content,
+  };
+};
+
+export const renderMarkdown = async (
+  markdownContent: string
+): Promise<string> => {
+  return await markdownToHtml(markdownContent || '');
+};
