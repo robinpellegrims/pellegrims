ce936dbc83478a22996854b2efc61bb02efb4d1a
robinpel <robin.pellegrims@gmail.com>
2021-10-17 10:28:08 +0200

    initial commit

---
+++ apps/pellegrims-dev/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'pellegrims-dev',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/pellegrims-dev',
+};
