a42e556371d81586ea1b0ccd9ba9c0cd424a6edd
robinpel <robin.pellegrims@gmail.com>
2021-11-01 17:07:27 +0100

    feat(markdown): open external links in blank target

--- libs/markdown/src/lib/markdown-to-html.ts
+++ libs/markdown/src/lib/markdown-to-html.ts
@@ -1,7 +1,11 @@
 import { remark } from 'remark';
 import remarkHtml from 'remark-html';
+import externalLinks from 'remark-external-links';
 
 export async function markdownToHtml(markdown) {
-  const result = await remark().use(remarkHtml).process(markdown);
+  const result = await remark()
+    .use(externalLinks, { target: '_blank', rel: ['nofollow'] })
+    .use(remarkHtml)
+    .process(markdown);
   return result.toString();
 }
