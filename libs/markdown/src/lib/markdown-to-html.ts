f3d654e238441e3356edce8e29e73863285612ee
robinpel <robin.pellegrims@gmail.com>
2021-11-04 22:34:25 +0100

    fix(pellegrims-dev): links to cross-origin destinations are unsafe

--- libs/markdown/src/lib/markdown-to-html.ts
+++ libs/markdown/src/lib/markdown-to-html.ts
@@ -4,7 +4,7 @@ import externalLinks from 'remark-external-links';
 
 export async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(externalLinks, { target: '_blank', rel: ['nofollow'] })
+    .use(externalLinks, { target: '_blank', rel: ['noreferrer'] })
     .use(remarkHtml)
     .process(markdown);
   return result.toString();
