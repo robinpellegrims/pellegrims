7c159f8f4fa8482ee914b162d3aace99d7915851
robinpel <robin.pellegrims@gmail.com>
2021-11-06 16:14:05 +0100

    feat(nx-remotecache-s3): initial implementation

---
+++ libs/nx-remotecache-s3/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'nx-remotecache-s3',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/nx-remotecache-s3',
+};
