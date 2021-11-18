1965bb96c10ae3226aed56bb5b2eb181bd837bef
robinpel <robin.pellegrims@gmail.com>
2021-11-18 20:22:02 +0100

    feat(eslint-config): add @angular-eslint/template/process-inline-templates

--- libs/eslint-config/src/lib/angular-eslint.ts
+++ libs/eslint-config/src/lib/angular-eslint.ts
@@ -1,6 +1,9 @@
 export = {
   plugins: ['@angular-eslint'],
-  extends: ['@angular-eslint/recommended'],
+  extends: [
+    '@angular-eslint/recommended',
+    'plugin:@angular-eslint/template/process-inline-templates',
+  ],
   rules: {
     '@angular-eslint/use-lifecycle-interface': 'error',
     '@angular-eslint/use-component-view-encapsulation': 'error',
