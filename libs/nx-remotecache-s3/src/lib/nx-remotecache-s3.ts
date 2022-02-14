af833f4e78aad7d1d5e71cfae531f6855d66ce1c
hoonoh <hoonoh@users.noreply.github.com>
2022-02-14 12:28:59 +0900

    feat(nx-remotecache-s3): add prefix path option

--- libs/nx-remotecache-s3/src/lib/nx-remotecache-s3.ts
+++ libs/nx-remotecache-s3/src/lib/nx-remotecache-s3.ts
@@ -6,6 +6,7 @@ import { Stream } from 'stream';
 const ENV_ACCESS_KEY_ID = 'NX_CACHE_S3_ACCESS_KEY_ID';
 const ENV_SECRET_KEY = 'NX_CACHE_S3_SECRET_KEY';
 const ENV_BUCKET = 'NX_CACHE_S3_BUCKET';
+const ENV_PREFIX = 'NX_CACHE_S3_PREFIX';
 const ENV_ENDPOINT = 'NX_CACHE_S3_ENDPOINT';
 const ENV_REGION = 'NX_CACHE_S3_REGION';
 
@@ -13,6 +14,7 @@ const getEnv = (key: string) => process.env[key];
 
 interface S3Options {
   bucket?: string;
+  prefix?: string;
   endpoint?: string;
   region?: string;
   accessKeyId?: string;
@@ -40,6 +42,8 @@ export default createCustomRunner<S3Options>(async (options) => {
 
   const bucket = getEnv(ENV_BUCKET) ?? options.bucket;
 
+  const prefix = getEnv(ENV_PREFIX) ?? options.prefix ?? '';
+
   return {
     name: 'S3',
     fileExists: async (filename) => {
@@ -47,7 +51,7 @@ export default createCustomRunner<S3Options>(async (options) => {
         const result = await s3Storage.headObject({
           /* eslint-disable @typescript-eslint/naming-convention */
           Bucket: bucket,
-          Key: filename,
+          Key: `${prefix}${filename}`,
           /* eslint-enable @typescript-eslint/naming-convention */
         });
         return !!result;
@@ -63,7 +67,7 @@ export default createCustomRunner<S3Options>(async (options) => {
       const result = await s3Storage.getObject({
         /* eslint-disable @typescript-eslint/naming-convention */
         Bucket: bucket,
-        Key: filename,
+        Key: `${prefix}${filename}`,
         /* eslint-enable @typescript-eslint/naming-convention */
       });
       return getStream.buffer(result.Body as Stream);
@@ -72,7 +76,7 @@ export default createCustomRunner<S3Options>(async (options) => {
       await s3Storage.putObject({
         /* eslint-disable @typescript-eslint/naming-convention */
         Bucket: bucket,
-        Key: filename,
+        Key: `${prefix}${filename}`,
         Body: buffer,
         /* eslint-enable @typescript-eslint/naming-convention */
       }),
