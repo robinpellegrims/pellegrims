072f2e387cd662f1313eb3aa57026d4b97bb17e8
robinpel <robin.pellegrims@gmail.com>
2022-04-19 21:11:06 +0200

    test: collect coverage from untested files

--- jest.preset.js
+++ jest.preset.js
@@ -3,5 +3,6 @@ const nxPreset = require('@nrwl/jest/preset');
 module.exports = {
   ...nxPreset,
   collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/**/*.{js,jsx,ts,tsx}'],
   coverageReporters: ['json'],
 };
