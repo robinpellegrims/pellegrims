2c7d755d309d505b394eb051e20bd598f177819b
robinpel <robin.pellegrims@gmail.com>
2022-04-24 15:34:31 +0200

    style(repo): commitlint allow release type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,27 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['repo', 'pellegrims-dev', 'eslint-config', 'nx-remotecache-s3'],
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+      ],
+    ],
+  },
+};
