abc64e7a1764f9dbfd4bb289e63b542179715f4b
robinpel <robin.pellegrims@gmail.com>
2022-06-07 23:39:35 +0200

    refactor(pellegrims-dev): extract twitterHandle constant

--- apps/pellegrims-dev/constants.ts
+++ apps/pellegrims-dev/constants.ts
@@ -22,7 +22,8 @@ export const linkedInUrl = 'https://www.linkedin.com/in/robinpellegrims';
 export const twitterSvgIcon =
   'M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z';
 export const twitterUrl = 'https://twitter.com/robinpel';
-export const twitterHandle = '@robinpel';
+export const twitterUserName = 'robinpel';
+export const twitterHandle = `@${twitterUserName}`;
 export const productionUrl = 'https://www.pellegrims.dev';
 const firstName = 'Robin';
 const lastName = 'Pellegrims';
