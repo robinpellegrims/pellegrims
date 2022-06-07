9a748b64e2b9a48d92767014c5ce7a4dc02f81d1
robinpel <robin.pellegrims@gmail.com>
2022-06-07 22:58:51 +0200

    feat(pellegrims-dev): [blog] support markdown images

---
+++ apps/pellegrims-dev/components/markdown-image.tsx
@@ -0,0 +1,50 @@
+import { FunctionComponent } from 'react';
+import Image from 'next/image';
+import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
+
+const isDefined = <SomeType,>(
+  val: SomeType | undefined | null
+): val is SomeType => val !== undefined && val !== null;
+
+const isString = (value: unknown): value is string => typeof value === 'string';
+
+const MarkdownImage: FunctionComponent<
+  JSX.IntrinsicElements['img'] & ReactMarkdownProps
+> = ({ node, children }) => {
+  if (
+    isDefined(node.properties) &&
+    isString(node.properties.alt) &&
+    isString(node.properties.src)
+  ) {
+    const metastring = node.properties.alt;
+    const alt = metastring.replace(/ *\{[^)]*} */g, '');
+    const metaWidth = metastring.match(/{([^}]+)x/);
+    const metaHeight = metastring.match(/x([^}]+)}/);
+    const width = metaWidth?.[1] ?? '100%';
+    const height = metaHeight?.[1] ?? '100%';
+    const isPriority = metastring.toLowerCase().match('{priority}') !== null;
+    const hasCaption = metastring?.toLowerCase().includes('{caption:');
+    const caption = metastring?.match(/{caption: (.*?)}/)?.pop();
+    const imageSrc = node.properties.src;
+
+    return (
+      <span className="block text-center">
+        <Image
+          src={imageSrc}
+          alt={alt}
+          priority={isPriority}
+          width={width}
+          height={height}
+        />
+        {hasCaption ? (
+          <div className="caption" aria-label={caption}>
+            {caption}
+          </div>
+        ) : null}
+      </span>
+    );
+  }
+  return <p>{children}</p>;
+};
+
+export default MarkdownImage;
--- apps/pellegrims-dev/components/markdown.tsx
+++ apps/pellegrims-dev/components/markdown.tsx
@@ -2,13 +2,14 @@ import ReactMarkdown from 'react-markdown';
 import { MarkdownDocument } from '@pellegrims/markdown';
 import CodeBlock from './code-block';
 import { FunctionComponent } from 'react';
+import MarkdownImage from './markdown-image';
 
 interface MarkdownProps {
   markDown: MarkdownDocument;
 }
 
 const Markdown: FunctionComponent<MarkdownProps> = ({ markDown }) => (
-  <ReactMarkdown components={{ code: CodeBlock }}>
+  <ReactMarkdown components={{ code: CodeBlock, img: MarkdownImage }}>
     {markDown.content}
   </ReactMarkdown>
 );
