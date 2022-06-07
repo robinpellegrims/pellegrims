b15fefbe72331198510c2749e1c1bf8d28743878
robinpel <robin.pellegrims@gmail.com>
2022-06-07 22:58:00 +0200

    feat(pellegrims-dev): [blog] support inline code fragments

--- apps/pellegrims-dev/components/code-block.tsx
+++ apps/pellegrims-dev/components/code-block.tsx
@@ -3,9 +3,15 @@ import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { FunctionComponent } from 'react';
 
-const CodeBlock: FunctionComponent<CodeProps> = ({ className, children }) => {
+const CodeBlock: FunctionComponent<CodeProps> = ({
+  className,
+  children,
+  inline,
+}) => {
   const language = className?.split('-')[1];
-  return (
+  return inline ? (
+    <code className={className}>{children}</code>
+  ) : (
     <SyntaxHighlighter
       language={language}
       showLineNumbers={true}
