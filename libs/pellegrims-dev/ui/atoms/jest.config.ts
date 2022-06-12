075e1aff529e7a3f36ce6ed902f422861f18f977
robinpel <robin.pellegrims@gmail.com>
2022-06-12 14:30:47 +0200

    feat(pellegrims-dev): add atoms lib

---
+++ libs/pellegrims-dev/ui/atoms/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'pellegrims-dev-ui-atoms',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/pellegrims-dev/ui/atoms',
+};
