d739aea19e67712e0cd6b45addbfc950021cabe7
robinpel <robin.pellegrims@gmail.com>
2022-06-12 13:43:34 +0200

    refactor(pellegrims-dev): use functional component for document

--- apps/pellegrims-dev/pages/_document.tsx
+++ apps/pellegrims-dev/pages/_document.tsx
@@ -1,25 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 import { Favicon } from '../components/favicon';
 
-class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html className="bg-white dark:bg-dark-800 text-dark-900 dark:text-dark-100">
-        <Head>
-          <meta charSet="utf-8" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Caveat&display=swap"
-            rel="stylesheet"
-          />
-          <Favicon />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export const MyDocument = () => (
+  <Html className="bg-white dark:bg-dark-800 text-dark-900 dark:text-dark-100">
+    <Head>
+      <meta charSet="utf-8" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Caveat&display=swap"
+        rel="stylesheet"
+      />
+      <Favicon />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 export default MyDocument;
