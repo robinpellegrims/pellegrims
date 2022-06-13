1177e9812fbe3156d08776bc29a0587445f31235
robinpel <robin.pellegrims@gmail.com>
2022-06-13 22:02:42 +0200

    feat(pellegrims-dev): add molecules lib

---
+++ libs/pellegrims-dev/ui/molecules/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'pellegrims-dev-ui-molecules',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/pellegrims-dev/ui/molecules',
+};
---
+++ libs/pellegrims-dev/ui/molecules/src/lib/pellegrims-dev-ui-molecules.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface PellegrimsDevUiMoleculesProps {}
+
+export function PellegrimsDevUiMolecules(
+  _props: PellegrimsDevUiMoleculesProps
+) {
+  return (
+    <div>
+      <h1>Welcome to PellegrimsDevUiMolecules!</h1>
+    </div>
+  );
+}
+
+export default PellegrimsDevUiMolecules;
