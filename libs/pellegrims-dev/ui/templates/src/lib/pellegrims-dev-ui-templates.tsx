e32ae7c0b4bfe4a4f2d2068afa701822b2b76599
robinpel <robin.pellegrims@gmail.com>
2022-06-17 12:43:31 +0200

    feat(pellegrims-dev): add templates lib

---
+++ libs/pellegrims-dev/ui/templates/src/lib/pellegrims-dev-ui-templates.tsx
@@ -0,0 +1,9 @@
+export function PellegrimsDevUiTemplates() {
+  return (
+    <div>
+      <h1>Welcome to PellegrimsDevUiTemplates!</h1>
+    </div>
+  );
+}
+
+export default PellegrimsDevUiTemplates;
