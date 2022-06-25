53183d5897fdcd1da62c3bc438b27f8381a24369
robinpel <robin.pellegrims@gmail.com>
2022-06-25 16:26:22 +0200

    feat(pellegrims-dev): link article summary title to content

--- libs/pellegrims-dev/ui/organisms/src/lib/blog-article-summary/blog-article-summary.tsx
+++ libs/pellegrims-dev/ui/organisms/src/lib/blog-article-summary/blog-article-summary.tsx
@@ -11,21 +11,28 @@ export interface BlogArticleSummaryProps {
 
 export const BlogArticleSummary: FunctionComponent<BlogArticleSummaryProps> = (
   props
-) => (
-  <article className="flex flex-col md:flex-row md:flex-nowrap gap-12">
-    <div className="md:w-1/5 flex-shrink-0 flex flex-col gap-2">
-      <DateFormatted date={props.post.frontMatter.date} />
-      <Tags tags={props.post.frontMatter.tags} />
-    </div>
-    <div className="flex flex-col gap-6 md:flex-grow prose lg:prose-xl dark:prose-invert">
-      <h2>{props.post.frontMatter.title}</h2>
-      <p className="leading-relaxed">{props.post.frontMatter.description}</p>
-      <Link href={`${props.path}/${props.post.slug}`}>
-        <a className="inline-flex items-center">
-          Read more
-          <RightArrow />
-        </a>
-      </Link>
-    </div>
-  </article>
-);
+) => {
+  const articleHref = `${props.path}/${props.post.slug}`;
+  return (
+    <article className="flex flex-col md:flex-row md:flex-nowrap gap-12">
+      <div className="md:w-1/5 flex-shrink-0 flex flex-col gap-2">
+        <DateFormatted date={props.post.frontMatter.date} />
+        <Tags tags={props.post.frontMatter.tags} />
+      </div>
+      <div className="flex flex-col gap-6 md:flex-grow prose lg:prose-xl dark:prose-invert">
+        <h2>
+          <Link href={articleHref}>
+            <a>{props.post.frontMatter.title}</a>
+          </Link>
+        </h2>
+        <p className="leading-relaxed">{props.post.frontMatter.description}</p>
+        <Link href={articleHref}>
+          <a className="inline-flex items-center">
+            Read more
+            <RightArrow />
+          </a>
+        </Link>
+      </div>
+    </article>
+  );
+};
