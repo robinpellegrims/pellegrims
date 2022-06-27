a2e46bdf02bd23dd9907510c21d452741aa2a20b
robinpel <robin.pellegrims@gmail.com>
2022-06-28 00:39:11 +0200

    fix(pellegrims-dev): canonical url misses ending slash

--- apps/pellegrims-dev/utils/url.ts
+++ apps/pellegrims-dev/utils/url.ts
@@ -3,7 +3,7 @@ import { productionUrl } from '../constants';
 export const buildRelativeBlogArticleUrl = (slug: string) => `/blog/${slug}`;
 
 export const buildCanonicalBlogArticleUrl = (slug: string) =>
-  `${productionUrl}${buildRelativeBlogArticleUrl(slug)}`;
+  `${productionUrl}${buildRelativeBlogArticleUrl(slug)}/`;
 
 export interface OGImageParams {
   title?: string;
