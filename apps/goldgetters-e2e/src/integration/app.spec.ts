b3483cbac83a127f9bb39c44eb000f6a0db79499
robinpel <robin.pellegrims@gmail.com>
2022-08-07 18:46:51 +0200

    feat(goldgetters): integrate tailwindcss

--- apps/goldgetters-e2e/src/integration/app.spec.ts
+++ apps/goldgetters-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('goldgetters', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome goldgetters');
+    cy.get('img[alt="ZVC Goldgetters logo"]').should('be.visible');
   });
 });
