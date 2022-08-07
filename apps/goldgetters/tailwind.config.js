ee57d9b63eb4f59964fa042b5171d40a772ebe9d
robinpel <robin.pellegrims@gmail.com>
2022-08-07 18:46:51 +0200

    feat(goldgetters): integrate tailwindcss

---
+++ apps/goldgetters/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: { extend: {} },
+  plugins: [require('@tailwindcss/typography')],
+};
