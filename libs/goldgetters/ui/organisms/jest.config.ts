93f9773c0fc3d1d4be3e0b4951935e72061f53e2
robinpel <robin.pellegrims@gmail.com>
2022-08-13 15:42:03 +0200

    feat(goldgetters): add ui-organisms library

---
+++ libs/goldgetters/ui/organisms/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'goldgetters-ui-organisms',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/goldgetters/ui/organisms',
+};
