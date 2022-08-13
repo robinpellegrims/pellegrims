ce38d6392fd1679b5c6a9db112bff7b809baa823
robinpel <robin.pellegrims@gmail.com>
2022-08-13 16:13:06 +0200

    feat(goldgetters): add templates library

---
+++ libs/goldgetters/ui/templates/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'goldgetters-ui-templates',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/goldgetters/ui/templates',
+};
---
+++ libs/goldgetters/ui/templates/src/lib/goldgetters-ui-templates.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface GoldgettersUiTemplatesProps {}
+
+export function GoldgettersUiTemplates(props: GoldgettersUiTemplatesProps) {
+  return (
+    <div>
+      <h1>Welcome to GoldgettersUiTemplates!</h1>
+    </div>
+  );
+}
+
+export default GoldgettersUiTemplates;
