a21794378ea0cf3a514188867f9b79ba43f2aefe
robinpel <robin.pellegrims@gmail.com>
2022-08-13 16:56:44 +0200

    feat(goldgetters): use page template

--- apps/goldgetters/pages/_app.tsx
+++ apps/goldgetters/pages/_app.tsx
@@ -2,19 +2,20 @@ import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CounterDevAnalytics } from '@pellegrims/shared/ui/atoms';
+import { PageTemplate } from '@pellegrims/goldgetters/ui/templates';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>ZVC Goldgetters</title>
-      </Head>
-      <CounterDevAnalytics user="RobinPel" />
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
-}
+const CustomApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>ZVC Goldgetters</title>
+    </Head>
+    <CounterDevAnalytics user="RobinPel" />
+    <PageTemplate
+      header={<></>}
+      content={<Component {...pageProps} />}
+      footer={<></>}
+    />
+  </>
+);
 
 export default CustomApp;
