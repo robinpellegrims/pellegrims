684667011383a7fce36a9ce2154c94976c58d6a6
robinpel <robin.pellegrims@gmail.com>
2022-08-13 15:52:12 +0200

    feat(goldgetters): add counter.dev analytics

--- apps/goldgetters/pages/_app.tsx
+++ apps/goldgetters/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { CounterDevAnalytics } from '@pellegrims/shared/ui/atoms';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>ZVC Goldgetters</title>
       </Head>
+      <CounterDevAnalytics user="RobinPel" />
       <main>
         <Component {...pageProps} />
       </main>
