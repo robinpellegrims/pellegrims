f2dfc48e403198f9fec6ccfc7ef6ecf344b50e26
robinpel <robin.pellegrims@gmail.com>
2022-08-13 16:14:41 +0200

    feat(goldgetters): add molecules library

---
+++ libs/goldgetters/ui/molecules/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'goldgetters-ui-molecules',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/goldgetters/ui/molecules',
+};
---
+++ libs/goldgetters/ui/molecules/src/lib/goldgetters-ui-molecules.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface GoldgettersUiMoleculesProps {}
+
+export function GoldgettersUiMolecules(props: GoldgettersUiMoleculesProps) {
+  return (
+    <div>
+      <h1>Welcome to GoldgettersUiMolecules!</h1>
+    </div>
+  );
+}
+
+export default GoldgettersUiMolecules;
