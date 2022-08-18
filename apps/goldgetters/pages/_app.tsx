af71a2d79e4152c73bffca5cacd5425c83e9a155
robinpel <robin.pellegrims@gmail.com>
2022-08-18 23:27:47 +0200

    feat(goldgetters): integrate trpc

--- apps/goldgetters/pages/_app.tsx
+++ apps/goldgetters/pages/_app.tsx
@@ -1,10 +1,13 @@
 import 'tailwindcss/tailwind.css';
-import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CounterDevAnalytics } from '@pellegrims/shared/ui/atoms';
 import { Header } from '@pellegrims/goldgetters/ui/organisms';
 import { PageTemplate } from '@pellegrims/goldgetters/ui/templates';
 import logo from '../public/logo.png';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from './api/trpc/[trpc]';
+import { getBaseUrl } from '../utils/url';
 
 const headerNavLinks: { text: string; href: string }[] = [
   { text: 'Nieuws', href: '/news' },
@@ -14,7 +17,7 @@ const headerNavLinks: { text: string; href: string }[] = [
   { text: 'Contact', href: '/contact' },
 ];
 
-const CustomApp = ({ Component, pageProps }: AppProps) => (
+const GoldgettersApp: AppType = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>ZVC Goldgetters</title>
@@ -28,4 +31,6 @@ const CustomApp = ({ Component, pageProps }: AppProps) => (
   </>
 );
 
-export default CustomApp;
+export default withTRPC<AppRouter>({
+  config: () => ({ url: `${getBaseUrl()}/api/trpc` }),
+})(GoldgettersApp);
