804f20b31021697e033cf21d72466135d0322516
robinpel <robin.pellegrims@gmail.com>
2022-08-22 23:50:46 +0200

    refactor(goldgetters): use icon component

--- libs/goldgetters/ui/atoms/src/lib/icon/icon.tsx
+++ libs/goldgetters/ui/atoms/src/lib/icon/icon.tsx
@@ -1,10 +1,10 @@
 import { FunctionComponent, ReactElement } from 'react';
 import {
-  MenuIcon,
+  Bars3Icon,
   MoonIcon,
   SunIcon,
   UserIcon,
-} from '@heroicons/react/outline';
+} from '@heroicons/react/24/outline';
 
 export type IconType = 'sun' | 'moon' | 'user' | 'hamburger';
 
@@ -16,7 +16,7 @@ const iconMapping: Record<IconType, ReactElement> = {
   moon: <MoonIcon className="h-6 w-6" />,
   sun: <SunIcon className="h-6 w-6" />,
   user: <UserIcon className="h-6 w-6" />,
-  hamburger: <MenuIcon className="h-6 w-6" />,
+  hamburger: <Bars3Icon className="h-6 w-6" />,
 };
 
 export const Icon: FunctionComponent<IconProps> = ({ type }) =>
