b4727d7ef8ba5919be69a73c36e146b486428f6e
robinpel <robin.pellegrims@gmail.com>
2022-08-28 18:39:28 +0200

    feat(goldgetters): integrate footer component

---
+++ apps/goldgetters/constants.ts
@@ -0,0 +1,3 @@
+export const facebookUrl = 'https://www.facebook.com/Goldgetters';
+export const githubUrl = 'https://github.com/robinpellegrims/pellegrims';
+export const twitterUrl = 'https://twitter.com/ZVCGoldgetters';
