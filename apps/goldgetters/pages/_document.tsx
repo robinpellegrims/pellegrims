cdb1792a39f9320ec23536fd8c85bd9058e9d037
robinpel <robin.pellegrims@gmail.com>
2022-09-16 21:09:11 +0200

    refactor(goldgetters): move favicon to shared-ui-atoms

--- apps/goldgetters/pages/_document.tsx
+++ apps/goldgetters/pages/_document.tsx
@@ -1,9 +1,10 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { FavIcon } from '@pellegrims/shared/ui/atoms';
 
 export const MyDocument = () => (
   <Html lang="nl">
     <Head>
-      <FavIcon />
+      <FavIcon pathPrefix="/favicon" />
     </Head>
     <body className="dark:bg-dark-800 text-dark-900 dark:text-dark-100 bg-white">
       <Main />
@@ -13,35 +14,3 @@ export const MyDocument = () => (
 );
 
 export default MyDocument;
-
-const FavIcon = () => (
-  <>
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicon/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon/favicon-16x16.png"
-    />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
-    <link
-      rel="mask-icon"
-      href="/favicon/safari-pinned-tab.svg"
-      color="#5bbad5"
-    />
-    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-    <meta name="theme-color" content="#ffffff" />
-  </>
-);
--- libs/shared/ui/atoms/src/index.ts
+++ libs/shared/ui/atoms/src/index.ts
@@ -1,2 +1,3 @@
 export { NextLink } from './lib/next-link/next-link';
 export { CounterDevAnalytics } from './lib/counter-dev-analytics';
+export { FavIcon } from './lib/favicon/favicon';
