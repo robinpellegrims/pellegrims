518320ca774df86e9adb9c80881fe1c48aa35652
robinpel <robin.pellegrims@gmail.com>
2022-12-10 16:40:18 +0100

    refactor(nx-remotecache-s3): move task runner implementation in separate file

--- libs/nx-remotecache-s3/src/lib/nx-remotecache-s3.spec.ts
+++ libs/nx-remotecache-s3/src/lib/nx-remotecache-s3.spec.ts
@@ -1,27 +1,5 @@
 import runner from './nx-remotecache-s3';
-import { performance } from 'perf_hooks';
 
-describe('nx-remotecache-s3', () => {
-  beforeEach(() => {
-    performance.mark('init-local');
-    performance.mark('command-execution-begins');
-  });
-
-  test('should have a defined runner', async () => {
-    const s3Runner = await runner(
-      [],
-      { lifeCycle: { startCommand: jest.fn(), endCommand: jest.fn() } },
-      {
-        taskGraph: {
-          tasks: {},
-          dependencies: {},
-          roots: [],
-        },
-        nxArgs: {},
-        nxJson: { npmScope: 'proj' },
-        projectGraph: { nodes: {}, dependencies: {} },
-      }
-    );
-    expect(s3Runner).toBeDefined();
-  });
-});
+describe('nx-remotecache-s3', () =>
+  test('should have a defined runner', async () =>
+    expect(runner).toBeInstanceOf(Function)));
