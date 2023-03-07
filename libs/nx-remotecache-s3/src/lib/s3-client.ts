b9da95e852c1a7e3bd65d064be1bf4047f1db32a
Luke Ramsden <luke.ramsden@transficc.com>
2023-03-07 15:32:41 +0000

    feat: add environment variable to explicitly set custom access key ID and secret access key for AWS

--- libs/nx-remotecache-s3/src/lib/s3-client.ts
+++ libs/nx-remotecache-s3/src/lib/s3-client.ts
@@ -5,20 +5,35 @@ import { getDefaultRoleAssumerWithWebIdentity } from '@aws-sdk/client-sts';
 
 import type { S3ClientConfig } from '@aws-sdk/client-s3/dist-types/S3Client';
 import type { DefaultProviderInit } from '@aws-sdk/credential-provider-node/dist-types/defaultProvider';
+import type { AwsCredentialIdentity, Provider } from '@aws-sdk/types';
 
 const ENV_ENDPOINT = 'NXCACHE_S3_ENDPOINT';
 const ENV_PROFILE = 'NXCACHE_S3_PROFILE';
 const ENV_FORCE_PATH_STYLE = 'NXCACHE_S3_FORCE_PATH_STYLE';
 const ENV_REGION = 'NXCACHE_S3_REGION';
+const ENV_ACCESS_KEY_ID = 'NXCACHE_S3_ACCESS_KEY_ID';
+const ENV_SECRET_ACCESS_KEY = 'NXCACHE_S3_SECRET_ACCESS_KEY';
 
 export const buildS3Client = (
   options: Pick<S3ClientConfig, 'endpoint' | 'region' | 'forcePathStyle'> &
     Pick<DefaultProviderInit, 'profile'>
 ) => {
-  const provider = defaultProvider({
-    profile: getEnv(ENV_PROFILE) ?? options.profile,
-    roleAssumerWithWebIdentity: getDefaultRoleAssumerWithWebIdentity(),
-  });
+  const awsAccessKeyIdOverride = getEnv(ENV_ACCESS_KEY_ID);
+  const awsSecretAccessKeyOverride = getEnv(ENV_SECRET_ACCESS_KEY);
+
+  let provider: AwsCredentialIdentity | Provider<AwsCredentialIdentity>;
+
+  if (awsAccessKeyIdOverride?.length && awsSecretAccessKeyOverride?.length) {
+    provider = {
+      accessKeyId: awsAccessKeyIdOverride,
+      secretAccessKey: awsSecretAccessKeyOverride,
+    };
+  } else {
+    provider = defaultProvider({
+      profile: getEnv(ENV_PROFILE) ?? options.profile,
+      roleAssumerWithWebIdentity: getDefaultRoleAssumerWithWebIdentity(),
+    });
+  }
 
   return new S3({
     endpoint: getEnv(ENV_ENDPOINT) ?? options.endpoint,
