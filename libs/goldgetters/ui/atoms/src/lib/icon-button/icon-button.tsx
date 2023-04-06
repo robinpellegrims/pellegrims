0cc1984e7b42940cbc29d2e5cf6cf88f11c203f3
robinpel <robin.pellegrims@gmail.com>
2023-04-06 21:38:12 +0200

    fix(goldgetters): icon button is gray instead of dark

--- libs/goldgetters/ui/atoms/src/lib/icon-button/icon-button.tsx
+++ libs/goldgetters/ui/atoms/src/lib/icon-button/icon-button.tsx
@@ -6,7 +6,7 @@ export const IconButton: FunctionComponent<
   <button
     onClick={onClick}
     type="button"
-    className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    className="text-dark-500 hover:bg-dark-100 focus:ring-dark-200 dark:text-dark-400 dark:hover:bg-dark-700 dark:focus:ring-dark-600 inline-flex items-center rounded-lg p-2 text-sm focus:outline-none focus:ring-2"
     aria-label={label}
   >
     {children}
