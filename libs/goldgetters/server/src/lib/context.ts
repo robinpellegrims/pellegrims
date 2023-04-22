02476f2aab6d21be8ee149f0f9112434f06869ac
robinpel <robin.pellegrims@gmail.com>
2023-04-22 22:49:14 +0200

    build(goldgetters): update next-auth to v4.22.1

--- libs/goldgetters/server/src/lib/context.ts
+++ libs/goldgetters/server/src/lib/context.ts
@@ -1,11 +1,11 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { goldgettersClient } from '@pellegrims/goldgetters/db';
-import { getServerSession } from './auth';
+import { getServerAuthSession } from './auth';
 
 export async function createContext(ctx: trpcNext.CreateNextContextOptions) {
   const { req, res } = ctx;
-  const session = await getServerSession(req, res);
+  const session = await getServerAuthSession(req, res);
 
   return {
     req,
