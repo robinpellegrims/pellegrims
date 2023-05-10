db30c50973f70fb1f8a90470d0786c4389d178c3
Steven Rathbauer <gh.rathpc@gmail.com>
2023-05-10 13:00:48 -0400

    chore: update Nx to v15.9.3 and run migrations

--- libs/goldgetters/data-access/jest.config.ts
+++ libs/goldgetters/data-access/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'goldgetters-data-access',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/goldgetters/data-access',
