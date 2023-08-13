5befba8d1df36853e46cec55901f834e232a9fa4
robinpel <robin.pellegrims@gmail.com>
2023-08-13 15:46:25 +0200

    build(goldgetters): exclude @swc modules from edge functions

--- apps/goldgetters/next.config.js
+++ apps/goldgetters/next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['node_modules/@swc/**/*'],
+    },
+  },
 };
 
 module.exports = withNx(nextConfig);
