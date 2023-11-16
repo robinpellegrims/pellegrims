6312a53e068a2a1a6fa3313443c9f0c2c3d60ce8
robinpellegrims <robin.pellegrims@gmail.com>
2023-11-16 20:24:11 +0100

    chore: remove cotersus app

--- apps/cotersus-e2e/src/e2e/app.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('cotersus', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display Cotersus', () => {
-    cy.get('h1').contains('Cotersus');
-    cy.get('h2').contains('IT Consulting');
-  });
-});
